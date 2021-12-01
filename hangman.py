6dcecdd6261538922b54ac5d180cbbab5eeb4178
Evgeny122 <95162969+Evgeny122@users.noreply.github.com>
2021-12-01 08:13:27 +0300

    Add files via upload

---
+++ hangman.py
@@ -0,0 +1,20 @@
+a = 'or'
+foo = '__'
+list_foo = list(foo)
+bar = ''
+# нашли схожую букву
+while a != foo:
+    n = input()
+    for baz in a:
+        if baz == n:
+# нашли индекс схожей буквы
+                for index, i in enumerate(a):
+                    if i == n:
+                        list_foo[index] = n
+                        foo = (''.join(list_foo))
+                        print(foo)
+        elif baz != n:
+            print('нет')
+                
+# вставляем букву в foo
+print('молодец')
