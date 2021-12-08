1da1953d4e6214dc0ea3b27675351e810756d78e
Evgeny122 <95162969+Evgeny122@users.noreply.github.com>
2021-12-08 08:51:46 +0300

    fixed

--- limited time.py
+++ limited time.py
@@ -1,14 +1,13 @@
 limited_time = 3
-user_input = '3'
-word = 'f'
-for i in word:
-    if i == user_input:
-        print('good')
-for i in word:
+while limited_time > 0:
+    user_input = input()
+    word = 'krek'
+    for i in word:
+        if i == user_input:
+            print('good')
+            break
     if i != user_input:     
         limited_time -= 1
         if limited_time == 0:
             print('вы проиграли')
-            break
         print(f'остлось {limited_time} попытки')
-        break
